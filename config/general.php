804c6addda0b8ddd648e46c7dcd9020cf989aef8
Absucc <57920717+absucc@users.noreply.github.com>
2021-03-30 20:22:04 -0300

    Create general.php

---
+++ config/general.php
@@ -0,0 +1,14 @@
+<?php
+$WEBSITE_NAME = "LOL";
+$WEBSITE_PROFILE_URL = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fexternal-preview.redd.it%2FTHXaRs1mL3UjphHLhX7gRce1PmqpkAPsx0IbAyozU1Y.jpg%3Fauto%3Dwebp%26s%3Dd8de39412fa423cb69ccb28012ff002914795edc&f=1&nofb=1";
+$WEBSITE_WEBSITE = "https://lucas.codeberg.page";
+$WEBSITE_DESCRIPTION = "A";
+$WEBSITE_EMAIL = "lol@lol";
+$WEBSITE_HTACCESS = False;
+
+// UTTERANCES (Comment section)
+$WEBSITE_UTTERANCES = True;
+$WEBSITE_UTTERANCES_SCRIPT = '<script src="https://utteranc.es/client.js" repo="absucc/comments" issue-term="url" theme="github-light" crossorigin="anonymous" async></script>';
+/* Get it on https://utteranc.es
+Example: <script src="https://utteranc.es/client.js" repo="[repo]" issue-term="url" theme="[theme]" crossorigin="anonymous" async></script> */
+?>
