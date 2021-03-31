2a4d3e64b86eb4ee42b669ce0a7dff09547b9b30
Absucc <57920717+absucc@users.noreply.github.com>
2021-03-31 19:52:23 -0300

    Update images.php

--- src/config/images.php
+++ src/config/images.php
@@ -1,34 +1,20 @@
 <?php
-/*
-First URL of the image, then name
-$data = [
-  [
-    "https://files.mastodon.online/media_attachments/files/105/963/772/571/166/206/original/97a401242b834375.jpg",
-    "LOL",
-
-  ]
-];
-*/
-$number_on_data = 4;
+/* First URL of the type of media (image/video), then the title/text of the content, later an image, and, if the content is a video, the video file
+The first thing in the variable is the oldest publication
+Don't forget to count all your posts and put the number in $number_on_data
+Please use correctly the ","
+Use the following example to make your own images.php
+Good luck! */
+$number_on_data = 2;
 $data = [
   [
     "image",
-    "LOL #funny",
+    "Test #funny",
     "https://files.mastodon.online/media_attachments/files/105/963/772/571/166/206/original/97a401242b834375.jpg"
   ],
   [
-    "image",
-    "Not funny ok",
-    "https://cloud.aspiechattr.me/media_attachments/files/105/922/173/826/230/666/original/32b1490217165848.jpeg"
-  ],
-  [
-    "image",
-    "Not funny #FUnny",
-    "https://cloud.aspiechattr.me/media_attachments/files/105/922/173/826/230/666/original/32b1490217165848.jpeg"
-  ],
-  [
     "video",
-    "Not funny ok",
+    "Test",
     "https://duckduckgo.com/i/045e1bff.jpg",
     "https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_720p_stereo.ogg"
   ]
