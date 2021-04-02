cc47c7ca2faf594a480123840a077e9fb04c9c46
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-02 12:33:01 -0300

    no more $number_on_data, welcome count($data)

--- src/index.php
+++ src/index.php
@@ -1,9 +1,9 @@
 <?php if(isset($_GET["lol"])){echo"<div style='text-align:center'><h1>Nanoiweb</h1>lololol</div>";}else{ include "config/general.php"; include "config/images.php"; $webtitle=$WEBSITE_NAME; $pub=False; include "includes/header.php";
-$a = $number_on_data -1;
+$a = count($data) -1;
 $ao = 0;
 $next = $a + 1;
 echo "<div class='feed'>\n";
-while($ao < $number_on_data){
+while($ao < count($data)){
   echo "<a href='$prefix_pub$next'><span class='mini'><img src='".$data[$a][2]."' title='".$data[$a][1]."'></span></a>";
   $a--; $ao++; $next--;
 }
