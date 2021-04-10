30c4d91c49e2cce0fcc03b796522d8ebe0ad859b
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-10 19:37:15 -0300

    Update footer.php

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,7 +1,7 @@
 
 </main>
 <br><hr>
-<footer>Pwd by <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.3 - <a href="rss.php">RSS</a></footer>
+<footer>Pwd by <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.3.1 - <a href="rss.php">RSS</a></footer>
 </div>
 </body>
 </html>
