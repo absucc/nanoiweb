f1d67053ffb41621ce18e476a8ee56b9b69be718
Absucc <57920717+absucc@users.noreply.github.com>
2021-09-18 20:44:40 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,10 @@
 <?php if(isset($_GET["lol"])){echo"<div style='text-align:center'><h1>Nanoiweb</h1>lololol</div>";}else{ include "config/general.php"; include "config/images.php"; $webtitle=$WEBSITE_NAME; $pub=False; include "includes/header.php";
-$a = count($data) -1;
+$a = count($data)-1;
 $ao = 0;
 $next = $a + 1;
 echo "<div class='feed'>\n";
 while($ao < count($data)){
-  if(strpos($data[$r][1],"'")){$title=str_replace("'", "\\'", $data[$a][1]);}else{$title=$data[$a][1];}
+  if(strpos($data[$a][1],"'")){$title=str_replace("'", "\\'", $data[$a][1]);}else{$title=$data[$a][1];}
   echo "<a href='$prefix_pub$next'><span class='mini'><img src='".$data[$a][2]."' title='$title'></span></a>";
   $a--; $ao++; $next--;
 }
